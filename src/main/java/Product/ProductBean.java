0935d1a15b1edf9aa73b3682b6d38057681244b2
ChoiHunJe <103643691+ChoiHunJE@users.noreply.github.com>
2022-05-31 16:48:18 +0900

    수정
    수정

---
+++ src/main/java/Product/ProductBean.java
@@ -0,0 +1,28 @@
+package Product;
+
+public class ProductBean {
+	private String productName;
+	private String productCompany;
+	private int productMoney;
+	
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductCompany() {
+		return productCompany;
+	}
+	public void setProductCompany(String productCompany) {
+		this.productCompany = productCompany;
+	}
+	public int getProductMoney() {
+		return productMoney;
+	}
+	public void setProductMoney(int productMoney) {
+		this.productMoney = productMoney;
+	}
+	
+	
+}
