da02f913fbe3d1e45592de085a2689fa45bfdb32
95Jiwon <95227922+95Jiwon@users.noreply.github.com>
2022-06-17 17:35:35 +0900

    22_06_17
    관리자 상품페이지01

--- src/main/java/Item/itemBean.java
+++ src/main/java/Item/itemBean.java
@@ -1,16 +1,67 @@
-package Item;
+package item;
 
-public class itemBean {
-	private String name;
+public class ItemBean {
+	private int itemNum;
+	private String itemName;
 	private int price;
+	private int sellPrice;
 	private int salePrice;
-	private String comment;
+	private String itemComment;
+	private String itemImage;
+	private String reg_date;
+	private int ref;
+	private int re_stop;
+	private int re_level;
+	private int readcount;
+	private String content;
 	
-	public String getName() {
-		return name;
+	public String getReg_date() {
+		return reg_date;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public int getRe_stop() {
+		return re_stop;
+	}
+	public void setRe_stop(int re_stop) {
+		this.re_stop = re_stop;
+	}
+	public int getRe_level() {
+		return re_level;
+	}
+	public void setRe_level(int re_level) {
+		this.re_level = re_level;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getItemNum() {
+		return itemNum;
+	}
+	public void setItemNum(int itemNum) {
+		this.itemNum = itemNum;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
 	}
 	public int getPrice() {
 		return price;
@@ -18,16 +69,28 @@ public class itemBean {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	public int getSellPrice() {
+		return sellPrice;
+	}
+	public void setSellPrice(int sellPrice) {
+		this.sellPrice = sellPrice;
+	}
 	public int getSalePrice() {
 		return salePrice;
 	}
 	public void setSalePrice(int salePrice) {
 		this.salePrice = salePrice;
 	}
-	public String getComment() {
-		return comment;
+	public String getItemComment() {
+		return itemComment;
 	}
-	public void setComment(String comment) {
-		this.comment = comment;
+	public void setItemComment(String itemComment) {
+		this.itemComment = itemComment;
+	}
+	public String getItemImage() {
+		return itemImage;
+	}
+	public void setItemImage(String itemImage) {
+		this.itemImage = itemImage;
 	}
 }
--- src/main/java/Item/itemDAO.java
+++ src/main/java/Item/itemDAO.java
@@ -1,42 +1,164 @@
-package Item;
+package item;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Vector;
 
-import Member.MemberBean;
+import item.ItemBean;
 
-public class itemDAO {
-	private Connection conn;
-	private ResultSet rs;
-	private PreparedStatement pstmt;
+public class ItemDAO {
 	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	//DB접속
 	public void getcon() {
 		try {
-			String dbURL="jdbc:mariadb://58.229.253.250:3306/student216";
-			String dbID ="student216";
-			String dbPassword ="1234!!";
+			String dbURL = "jdbc:mariadb://58.229.253.250:3306/student216";
+			String dbID = "student216";
+			String dbPassword = "1234!!";
 			Class.forName("org.mariadb.jdbc.Driver");
-			conn=DriverManager.getConnection(dbURL, dbID, dbPassword);
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void insertItem(itemBean ibean) {
+	public int insertItem(ItemBean ibean) {
+		int result =0;
+		getcon();
 		try {
-			String SQL = "INSERT INTO MEMBER(name, price, salePrice, comment) VALUES(?,?,?,?)";
+			
+			//기존에 item이 있는지 검사 하는 코드
+			// SQL = String SQL = "SELECT itemName FROM item WHERE itemNAME"
+			// if(getItemName.equlas(SQL){
+			//
+			// }
+			//result =1;
+			//없을 경우
+			String SQL = "INSERT INTO item(itemNum,itemName,price,sellPrice,salePrice,itemComment,itemImage) VALUES(?,?,?,?,?,?,?)";
 			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1, ibean.getName());
-			pstmt.setInt(2, ibean.getPrice());
-			pstmt.setInt(3, ibean.getSalePrice());
-			pstmt.setString(4, ibean.getComment());
+			pstmt.setInt(1, ibean.getItemNum()); //상품번호
+			pstmt.setString(2, ibean.getItemName()); //상품이름
+			pstmt.setInt(3, ibean.getPrice()); // 가격
+			pstmt.setInt(4, ibean.getSellPrice()); // 판매가
+			pstmt.setInt(5, ibean.getSalePrice()); //할인가
+			pstmt.setString(6, ibean.getItemComment()); //상품설명
+			pstmt.setString(7, ibean.getItemImage()); //상품이미지
 			pstmt.executeUpdate();
+			result = 0;
+			conn.close();
 		}catch(Exception e) {
 			e.printStackTrace();
+			result = -1;
 		}
+		
+		return result;
+		
 	}
 	
 	
+	//데이터베이스에 데이터 전달
+	public int upload(String itemNum, String itemName,int price, int sellPicce, int salePrice, String itemComment, String itemImage) {
+		getcon();
+		String sql = "insert into item values(?, ?, ?, ?, ?, ?, ?)";
+		int result;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, itemNum);
+			pstmt.setString(2, itemName);
+			pstmt.setInt(3, price);
+			pstmt.setInt(4, sellPicce);
+			pstmt.setInt(5, salePrice);
+			pstmt.setString(6, itemComment);
+			pstmt.setString(7, itemComment); //글의 유효번호
+			result = pstmt.executeUpdate();
+			conn.close();
+			
+			return result;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+	//
+	public int join(ItemBean item) {
+		  getcon();
+		  String sql = "insert into item values(?, ?, ?, ?, ?, ?)";
+		  try {
+		    pstmt = conn.prepareStatement(sql);
+		    pstmt.setString(1, item.getItemName());
+		    pstmt.setInt(2, item.getPrice());
+		    pstmt.setInt(3, item.getSellPrice());
+		    pstmt.setInt(4, item.getSalePrice());
+		    pstmt.setString(5, item.getItemComment());
+		    pstmt.setString(6, item.getItemImage());
+		    
+		    pstmt.executeUpdate();
+		    conn.close();
+		  }catch (Exception e) {
+		 	e.printStackTrace();
+		  }
+		  return -1;
+		}
+	
+	public int getAllCount() {
+		getcon();
+		int count = 0;
+		try {
+			String SQL = "select count(*) from board";
+			
+			pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
+	public Vector<ItemBean> getTotalAllBoard(){
+		Vector<ItemBean> v = new Vector<>();
+		getcon();
+		try {
+			String SQL ="select * from (select A.*,(@rownum := @rownum +1) as Rnum  FROM "
+					+ " (SELECT * from board ) AS A, "
+					+ " (SELECT @rownum:=0) tmp ) B  "
+					+ " ORDER BY num ASC";
+			pstmt = conn.prepareStatement(SQL);			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ItemBean bean = new ItemBean();
+				bean.setItemNum(rs.getInt(1));
+				bean.setItemName(rs.getString(2));
+				bean.setPrice(rs.getInt(3));
+				bean.setSellPrice(rs.getInt(4));
+				bean.setSalePrice(rs.getInt(5));
+				bean.setItemComment(rs.getString(6));
+				bean.setItemImage(rs.getString(7));
+				bean.setReg_date(rs.getDate(8).toString());
+				bean.setRef(rs.getInt(9));
+				bean.setRe_stop(rs.getInt(10));
+				bean.setRe_level(rs.getInt(11));
+				bean.setReadcount(rs.getInt(12));
+				bean.setContent(rs.getString(13));
+				
+				v.add(bean);
+			}
+			
+			conn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return v;
+	}
 }
